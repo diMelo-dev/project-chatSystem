cd17951bd688d674126f222e904731ac9d620e9a
diMelo-dev <danilo.osouza.melo@gmail.com>
2023-03-15 09:22:12 -0300

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "AIzaSyDoWCKwdEnjh2zV-ki-NFMdAuLK0iGRtiM",
-    authDomain: "chatsystem-a180f.firebaseapp.com",
-    projectId: "chatsystem-a180f",
-    storageBucket: "chatsystem-a180f.appspot.com",
-    messagingSenderId: "892594575607",
-    appId: "1:892594575607:web:d8679d347c39706a11dbf2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
